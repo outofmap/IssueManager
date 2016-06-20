bd36b5aa5dc172dc10584ef7b32daaf511d02fb4
outofmap <enjoyhee@gmail.com>
2016-06-20 18:14:00 +0900

    delete others

--- src/main/java/issueManager/controller/user/UserController.java
+++ src/main/java/issueManager/controller/user/UserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
